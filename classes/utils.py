6ccb2e6b4fa824ff7a913d7879babb3d3f87f8c9
Flesner96 <f.lesner@wp.pl>
2026-02-11 20:11:00 +0100

    fix schedule for more then one class the same time

--- classes/utils.py
+++ classes/utils.py
@@ -10,27 +10,22 @@ def generate_hour_slots(start=6, end=22):
 
 def build_hour_grid(events, hour_slots):
     """
-    Grid: { "HH:MM": event or None }
-
-    Jeden event na slot.
-    Jeśli event zaczyna się w środku godziny (np 17:30),
-    przypisujemy go do godziny startowej (17:00).
+    Grid: { "HH:MM": [event1, event2, ...] }
     """
 
     grid = {}
 
-    # inicjalizacja pustego gridu
+    # inicjalizacja pustych list
     for slot in hour_slots:
         key = slot.strftime("%H:%M")
-        grid[key] = None
+        grid[key] = []
 
-    # wrzucanie eventów
+    # przypisanie eventów
     for event in events:
-
-        start_hour = event.start_time.hour
-        key = f"{start_hour:02d}:00"
+        key = f"{event.start_time.hour:02d}:00"
 
         if key in grid:
-            grid[key] = event
+            grid[key].append(event)
 
     return grid
+
