3080d69e253d0415f260b7317f5b49f0864b26d6
Flesner96 <f.lesner@wp.pl>
2026-02-15 21:13:33 +0100

    fix url typo

--- vouchers/urls.py
+++ vouchers/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path ("", views.voucher_search.view, name="voucher_search"),
+    path ("", views.voucher_search_view, name="voucher_search"),
     path("create", views.voucher_create_view , name="voucher_create"),
 
 ]
